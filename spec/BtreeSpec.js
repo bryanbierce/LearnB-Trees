0b550bac7f6acb69ebef8e9d7b7f0475e6b4235a
Bryan Bierce <bryanbierce@gmail.com>
2015-11-24 11:22:07 -0800

    Moved testing functions into own file for clarity

--- spec/BtreeSpec.js
+++ spec/BtreeSpec.js
@@ -4,73 +4,6 @@ describe('Btree()', function () {
   'use strict';
   var bTree;
 
-  // This will do a depth first crawl through the tree
-  // It will check that the number of values at each node is less than the order
-  // It will return true if this holds for all nodes; otherwise it will return false
-  function sizeTest(bTree){
-    var size = true;
-    function traverseTree(bTree){
-      size = (bTree.values.length < bTree.order) ? size : false;
-      var childLength = btree.children.length;
-      if(childLength){
-        for(var i = 0; i < childLength; i++){
-          traverseTree(bTree.children[i]);
-        }
-      }
-    };
-
-    traverseTree(bTree);
-
-    return size;
-  };
-  // This will do a depth first crawl through the tree
-  // It will compare all branch end depths to the greatest depth yet found.
-  // It will then return true if the depth is even throught or false if not
-  function depthTest(bTree){
-    var depth = 0;
-    var maxDepth = 0;
-    var even = true;
-    function traverseTree(bTree){
-      depth++;
-      if(!childLength){
-        if(maxDepth === 0){
-          maxDepth = depth;
-        }else if(maxDepth !== depth){
-          even = false;
-        }
-        depth--;
-      }else{
-        for(var i = 0; i < childLength; i++){
-          traverseTree(bTree.children[i]);
-          depth--;
-        }
-      }
-    };
-
-    traverseTree(bTree);
-    return even;
-  };
-  // This will do a depth first crawl through the tree 
-  // It will compare the number of children to the order of the tree and check for equality
-  // It will return true if all nodes with children are in accordance with the order; otherwise it returns false 
-  function dispersionTest(bTree){
-    var order = true;
-    function traverseTree(bTree){
-      var childLength = bTree.children.length;
-      if(childLength){
-        order = (childLength === bTree.order) ? order : false;
-      }
-      if(order && childLength){
-        for(var i = 0; i < childLength; i++){
-          traverseTree(bTree.children[i]);
-        }
-      }
-    };
-
-    traverseTree(bTree);
-    return order;
-  };
-
   beforeEach(function (){
     bTree = new Btree(3);   
   });
@@ -111,29 +44,29 @@ describe('Btree()', function () {
     bTree.add(7);
     bTree.add(8);
     bTree.add(9);
-    expect(depthTest(btree)).to.equal(true);
+    expect(depthTest(bTree)).to.equal(true);
     bTree.remove(9);
-    expect(depthTest(btree)).to.equal(true);
+    expect(depthTest(bTree)).to.equal(true);
     bTree.add(9);
     bTree.add(10);
     bTree.add(11);
     bTree.add(12);
     bTree.add(13);
-    expect(depthTest(btree)).to.equal(true);
+    expect(depthTest(bTree)).to.equal(true);
   });
 
   it('if a node contains children the number of children should match the tree\'s order', function(){
     bTree.add(7);
     bTree.add(8);
     bTree.add(9);
-    expect(dispersionTest(btree)).to.equal(true);
+    expect(dispersionTest(bTree)).to.equal(true);
     bTree.remove(9);
-    expect(dispersionTest(btree)).to.equal(true);
+    expect(dispersionTest(bTree)).to.equal(true);
     bTree.add(9);
     bTree.add(10);
     bTree.add(11);
     bTree.add(12);
     bTree.add(13);
-    expect(dispersionTest(btree)).to.equal(true);
+    expect(dispersionTest(bTree)).to.equal(true);
   });
 });
